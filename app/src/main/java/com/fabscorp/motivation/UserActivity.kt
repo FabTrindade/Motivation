599ea78b727a9c9345da0748c3e76bfc1128058d
Fabricio Trindade <fabrtrindade@gmail.com>
2023-10-31 20:59:03 -0300

    Adding the button in UserLayout

--- app/src/main/java/com/fabscorp/motivation/UserActivity.kt
+++ app/src/main/java/com/fabscorp/motivation/UserActivity.kt
@@ -2,9 +2,10 @@ package com.fabscorp.motivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.fabscorp.motivation.databinding.ActivityUserBinding
 
-class UserActivity : AppCompatActivity() {
+class UserActivity : AppCompatActivity(), View.OnClickListener{
     private lateinit var bidding: ActivityUserBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,6 +13,12 @@ class UserActivity : AppCompatActivity() {
         bidding = ActivityUserBinding.inflate(layoutInflater)
         setContentView(bidding.root)
 
+        bidding.buttonSave.setOnClickListener(this)
         supportActionBar?.hide()
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save){
+        }
     }
 }
