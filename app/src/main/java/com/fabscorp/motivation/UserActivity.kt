85a421cb7bc03c6fea224fef39744183472aebf9
Fabricio Trindade <fabrtrindade@gmail.com>
2023-10-31 21:10:26 -0300

    Adding user name validation

--- app/src/main/java/com/fabscorp/motivation/UserActivity.kt
+++ app/src/main/java/com/fabscorp/motivation/UserActivity.kt
@@ -3,9 +3,10 @@ package com.fabscorp.motivation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.fabscorp.motivation.databinding.ActivityUserBinding
 
-class UserActivity : AppCompatActivity(), View.OnClickListener{
+class UserActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var bidding: ActivityUserBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +19,17 @@ class UserActivity : AppCompatActivity(), View.OnClickListener{
     }
 
     override fun onClick(v: View) {
-        if (v.id == R.id.button_save){
+        if (v.id == R.id.button_save) {
+            handleSave();
+        }
+    }
+
+    private fun handleSave() {
+        val name = bidding.editName.text.toString()
+
+        if (name != "") {
+        } else {
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_SHORT).show()
         }
     }
 }
