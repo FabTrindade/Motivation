090d00ab6ac10abc5e3c34ba2b4b1a324b6fcdbb
Fabricio Trindade <fabrtrindade@gmail.com>
2023-11-08 08:22:23 -0300

    Using constants as SharedPreferences parameters

---
+++ app/src/main/java/com/fabscorp/motivation/MotivationConstants.kt
@@ -0,0 +1,8 @@
+package com.fabscorp.motivation
+
+class MotivationConstants private constructor(){ //No one can instantiate this class
+
+    object KEY {
+        const val USER_NAME = "USER_NAME"
+    }
+}
