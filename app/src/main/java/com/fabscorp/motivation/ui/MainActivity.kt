23913cc37af023c29d14e6ebcd724dee1e27b643
Fabricio Trindade <fabrtrindade@gmail.com>
2023-11-10 08:17:10 -0300

    Handling category button clicks

--- app/src/main/java/com/fabscorp/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/fabscorp/motivation/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fabscorp.motivation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.fabscorp.motivation.infra.MotivationConstants
 import com.fabscorp.motivation.R
 import com.fabscorp.motivation.infra.SecurityPreferences
@@ -19,21 +20,44 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         supportActionBar?.hide()
 
         handleUserName()
+        listHandle(R.id.image_all)
 
         binding.buttonNewPhrase.setOnClickListener(this)
+        binding.imageAll.setOnClickListener(this)
+        binding.imageHappy.setOnClickListener(this)
+        binding.imageSunny.setOnClickListener(this)
 
 
     }
 
     private fun handleUserName() {
-        val name =  SecurityPreferences (this).getString(MotivationConstants.KEY.USER_NAME)
+        val name = SecurityPreferences(this).getString(MotivationConstants.KEY.USER_NAME)
         binding.textUserName.text = "Olá, $name!"
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.button_new_phrase) {
-            var s= "String"
+            var s = "String"
+        } else if (view.id in listOf(R.id.image_all, R.id.image_happy, R.id.image_sunny)) {
+            listHandle(view.id)
+        }
+    }
 
+    private fun listHandle(id: Int) {
+        binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+
+        when (id) {
+            R.id.image_all -> {
+                binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            }
+            R.id.image_happy -> {
+                binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            }
+            R.id.image_sunny -> {
+                binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            }
         }
     }
 }
